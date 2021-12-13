02fb1449ad7a148f97421ddf73e3f47a31d8b99b
luciano de franca <lucianodefranca86@gmail.com>
2021-12-13 19:16:40 -0300

    Project created

---
+++ src/main/java/com/luciano/coursespringboot/CourseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciano/coursespringboot/CourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
