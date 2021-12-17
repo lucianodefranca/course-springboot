f643294fa6d5e1ba97e032deb7bdf9b9c4888b41
luciano de franca <lucianodefranca86@gmail.com>
2021-12-16 22:01:57 -0300

    Payment, one-to-one association

--- src/main/java/com/luciano/coursespringboot/config/TestConfig.java
+++ src/main/java/com/luciano/coursespringboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luciano.coursespringboot.entities.Category;
 import com.luciano.coursespringboot.entities.Order;
 import com.luciano.coursespringboot.entities.OrderItem;
+import com.luciano.coursespringboot.entities.Payment;
 import com.luciano.coursespringboot.entities.Product;
 import com.luciano.coursespringboot.entities.User;
 import com.luciano.coursespringboot.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay);
+		
+		orderRepository.save(o1);
 	}
 	
 }
