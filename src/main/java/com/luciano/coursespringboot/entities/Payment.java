c8d3c470035e635ca2176188b88b7c81d8f8319a
luciano de franca <lucianodefranca86@gmail.com>
2021-12-16 22:14:11 -0300

    subTotal, Total methods

--- src/main/java/com/luciano/coursespringboot/entities/Payment.java
+++ src/main/java/com/luciano/coursespringboot/entities/Payment.java
@@ -23,7 +23,8 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-
+	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
