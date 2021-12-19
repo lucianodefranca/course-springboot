9aabf6f7f78472ff98963dba797c0c2e6ea22cfe
luciano de franca <lucianodefranca86@gmail.com>
2021-12-19 20:20:37 -0300

    Exception handling - delete

--- src/main/java/com/luciano/coursespringboot/service/UserService.java
+++ src/main/java/com/luciano/coursespringboot/service/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.luciano.coursespringboot.entities.User;
 import com.luciano.coursespringboot.repositories.UserRepository;
+import com.luciano.coursespringboot.service.exceptions.DataBaseException;
 import com.luciano.coursespringboot.service.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
---
+++ src/main/java/com/luciano/coursespringboot/service/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.luciano.coursespringboot.service.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
